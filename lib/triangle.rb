569fa3a8fbb98b2c6748e224f6384cdbfe6b22d8
Vladimir J <vladdaimpaler@protonmail.com>
2020-04-28 21:49:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
+require 'pry'
 class Triangle
-  # write code here
+attr_accessor :a, :b, :c  
+  
+  def initialize(side1,side2,side3)
+    @a = side1
+    @b = side2
+    @c = side3
+  end
+
+  def check
+    [@a,@b,@c].uniq.count
+  end
+
+  def kind
+    #binding.pry
+    return :equilateral if check == 1
+    return :isosceles if check == 2
+    return :scalene if check == 3
+  end
+  
+  class TriangleError < StandardError
+    
+  end
 end
